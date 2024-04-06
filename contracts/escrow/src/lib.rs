50929fb562d148832d68444c83588735190c1729
NanezX <victor.nanezj@gmail.com>
2024-04-06 05:20:40 -0400

    main escrow contract

--- contracts/escrow/src/lib.rs
+++ contracts/escrow/src/lib.rs
@@ -1,13 +1,41 @@
 #![no_std]
-use soroban_sdk::{contract, contracterror};
+mod storage;
+mod types;
 
-#[contracterror]
-#[derive(Copy, Clone, Debug, Eq, PartialEq, PartialOrd, Ord)]
-#[repr(u32)]
-pub enum Error {
-    OnlyOwner = 1,
-    OnlyOracle = 2,
-}
+use storage::Storage;
+use types::{DataKey, EscrowProposal, ProposalStatus};
+
+use soroban_sdk::{contract, contractimpl, log, BytesN, Env, Symbol};
 
 #[contract]
 pub struct EscrowContract;
+
+#[contractimpl]
+impl EscrowContract {
+    /**
+     * Register a new proposal to be picked,
+     */
+    pub fn add_proposal(env: Env, id: BytesN<32>) {
+        let propose_0 = DataKey::Proposal(id.clone()).has(&env);
+        log!(&env, "is_proposed_0:", propose_0);
+
+        let proposal: EscrowProposal = EscrowProposal {
+            id: id.clone(),
+            status: ProposalStatus::Actived,
+        };
+
+        DataKey::Proposal(id.clone()).set(&env, &proposal);
+
+        let propose_1 = DataKey::Proposal(id.clone()).has(&env);
+        log!(&env, "is_proposed_1:", propose_1);
+    }
+
+    /**
+     * This will pick an ID and offer XML
+     */
+    pub fn pick_proposal(env: Env, signaturit_id: BytesN<32>) {
+        //
+    }
+}
+
+mod test;
