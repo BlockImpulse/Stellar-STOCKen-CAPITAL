9e3efcd301d080ff84870d2ea1f021a19e902a7f
NanezX <victor.nanezj@gmail.com>
2024-04-06 01:22:22 -0400

    initial Escrow errors

--- contracts/escrow/src/lib.rs
+++ contracts/escrow/src/lib.rs
@@ -1,1 +1,13 @@
 #![no_std]
+use soroban_sdk::{contract, contracterror};
+
+#[contracterror]
+#[derive(Copy, Clone, Debug, Eq, PartialEq, PartialOrd, Ord)]
+#[repr(u32)]
+pub enum Error {
+    OnlyOwner = 1,
+    OnlyOracle = 2,
+}
+
+#[contract]
+pub struct EscrowContract;
