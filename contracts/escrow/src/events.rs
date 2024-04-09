fe84888e2ba2cb63bdcd8dd14ddd61f2c0d3534b
NanezX <victor.nanezj@gmail.com>
2024-04-09 19:49:51 -0400

    init even def

--- contracts/escrow/src/events.rs
+++ contracts/escrow/src/events.rs
@@ -8,5 +8,9 @@ pub const PROPOSAL_TOPIC: Symbol = symbol_short!("Proposal");
 /**
  * Register
  */
-
 pub const REGISTER_TOPIC: Symbol = symbol_short!("Register");
+
+/**
+ * Initialization event
+ */
+pub const INIT_TOPIC: Symbol = symbol_short!("INIT");
