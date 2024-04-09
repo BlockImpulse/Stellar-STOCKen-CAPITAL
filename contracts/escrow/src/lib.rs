d085c9099e7a1f6eb828d3a7a43ea435c55894e6
NanezX <victor.nanezj@gmail.com>
2024-04-09 04:23:49 -0400

    register escrow in contract

--- contracts/escrow/src/lib.rs
+++ contracts/escrow/src/lib.rs
@@ -3,9 +3,9 @@ mod events;
 mod storage;
 mod types;
 
-use events::PROPOSAL_TOPIC;
+use events::{PROPOSAL_TOPIC, REGISTER_TOPIC};
 use storage::Storage;
-use types::{DataKey, EscrowError, EscrowProposal, ProposalStatus};
+use types::{DataKey, EscrowError, EscrowProposal, ProposalStatus, SignatureTxEscrow};
 
 use soroban_sdk::{
     contract, contractimpl, panic_with_error, symbol_short, Address, Env, String, U256,
@@ -27,15 +27,12 @@ impl EscrowContract {
         proposer_address: Address,
         min_funds: U256,
     ) {
-        let escrow_id: u32 = DataKey::ProposalCounter.get(&env).unwrap_or(0);
-
-        if DataKey::Proposal(escrow_id.clone()).has(&env) {
+        if DataKey::Proposal(stocken_proposal_id.clone()).has(&env) {
             panic_with_error!(&env, EscrowError::AlreadyProposed);
         }
 
         let propose = EscrowProposal {
-            escrow_id: escrow_id.clone(),
-            stocken_proposal_id: stocken_proposal_id.clone(),
+            escrow_id: stocken_proposal_id.clone(),
             owner: proposer_address,
             status: ProposalStatus::Actived,
             min_funds,
@@ -45,11 +42,10 @@ impl EscrowContract {
             .publish((PROPOSAL_TOPIC, symbol_short!("Added")), propose.clone());
 
         // Save the proposal
-        DataKey::Proposal(escrow_id).set(&env, &propose);
-        DataKey::ProposalCounter.set(&env, &(escrow_id + 1));
+        DataKey::Proposal(stocken_proposal_id).set(&env, &propose);
     }
 
-    pub fn get_proposal(env: &Env, escrow_id: u32) -> EscrowProposal {
+    pub fn get_proposal(env: &Env, escrow_id: String) -> EscrowProposal {
         if !DataKey::Proposal(escrow_id.clone()).has(&env) {
             panic_with_error!(&env, EscrowError::ProposalNotFound);
         }
@@ -63,12 +59,39 @@ impl EscrowContract {
     the same UUID but with some hex values with a uppercase or lowercase, it
     will lead to the contract to identify it as two diff Ids.
     */
-    pub fn register_escrow(env: Env, signaturit_id: String, propose_id: u32, sender_id: Address) {
-        let proposer: EscrowProposal = Self::get_proposal(&env, propose_id);
+    pub fn register_escrow(env: Env, signaturit_id: String, sender_id: Address, funds: U256) {
+        let propose: EscrowProposal = Self::get_proposal(&env, signaturit_id.clone());
 
-        // Call the Oracle and get the oracle id to identify the tx escrow
+        if propose.status != ProposalStatus::Actived {
+            panic_with_error!(&env, EscrowError::PickedOrCanceled);
+        }
+
+        if propose.min_funds < funds {
+            panic_with_error!(&env, EscrowError::NoEnoughtFunds);
+        }
+
+        // Require auth of the sender to lock the funds
+        sender_id.require_auth();
+        // TODO: Move the funds to the contract
+
+        // Move the funds to here
+
+        // TODO: Call the Oracle and get the oracle id to identify the tx escrow
         // let oracle_id = oracle.register_new_sign(signaturit_id);
         let oracle_id = 0u32;
+
+        let tx_register = SignatureTxEscrow {
+            id: signaturit_id.clone(),
+            oracle_id,
+            buyer: sender_id,
+            receiver: propose.owner,
+            funds,
+        };
+
+        env.events()
+            .publish((REGISTER_TOPIC, symbol_short!("New")), tx_register.clone());
+
+        DataKey::SignatureProcess(signaturit_id).set(&env, &tx_register);
     }
 }
 
