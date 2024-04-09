ceaf3158db0957ac5c55fbe021f8e47ba76b66cf
NanezX <victor.nanezj@gmail.com>
2024-04-09 04:24:04 -0400

    event for register

--- contracts/escrow/src/events.rs
+++ contracts/escrow/src/events.rs
@@ -4,3 +4,9 @@ use soroban_sdk::{symbol_short, Symbol};
  * Proposal
  */
 pub const PROPOSAL_TOPIC: Symbol = symbol_short!("Proposal");
+
+/**
+ * Register
+ */
+
+pub const REGISTER_TOPIC: Symbol = symbol_short!("Register");
