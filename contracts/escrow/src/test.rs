c6ca4fbed449cec6fdbfaf74c63e76e4c23aa708
NanezX <victor.nanezj@gmail.com>
2024-04-17 01:54:33 -0400

    test export

--- contracts/escrow/src/test.rs
+++ contracts/escrow/src/test.rs
@@ -14,20 +14,23 @@ pub mod oracle {
     );
     pub type OracleClient<'a> = Client<'a>;
 }
+use oracle::OracleClient;
+
+fn create_oracle_contract<'a>(env: &Env) -> OracleClient<'a> {
+    let contract_id = env.register_contract_wasm(None, oracle::WASM);
+    let contract_client = OracleClient::new(env, &contract_id);
+    contract_client
+}
 
 pub mod escrow {
     soroban_sdk::contractimport!(file = "../../target/wasm32-unknown-unknown/release/escrow.wasm");
     pub type EscrowClient<'a> = Client<'a>;
 }
+use escrow::EscrowClient;
 
-fn create_escrow_contract<'a>(env: &Env) -> escrow::EscrowClient<'a> {
+fn create_escrow_contract<'a>(env: &Env) -> EscrowClient<'a> {
     let contract_id = env.register_contract_wasm(None, escrow::WASM);
-    escrow::EscrowClient::new(&env, &contract_id)
-}
-
-fn create_oracle_contract<'a>(env: &Env) -> oracle::OracleClient<'a> {
-    let contract_id = env.register_contract_wasm(None, oracle::WASM);
-    oracle::OracleClient::new(env, &contract_id)
+    EscrowClient::new(&env, &contract_id)
 }
 
 fn create_token_contract<'a>(
@@ -41,10 +44,15 @@ fn create_token_contract<'a>(
     )
 }
 
+// keccak256(STOCKEN_ID_1)
+pub const STOCKEN_ID_1: &str = "6ef7e237bbddb133bb3504cad9e2ec7ff90c0c9b63567a632dbad8bb2b923728";
+// keccak256(STOCKEN_ID_2)
+pub const STOCKEN_ID_2: &str = "af8f0b8ba4749d7a83edcd03a18e3ee3807fca630f8a18e8e59be53ea15c9e95";
+
 pub struct EscrowTest<'a> {
     env: Env,
-    escrow: escrow::EscrowClient<'a>,
-    oracle: oracle::OracleClient<'a>,
+    escrow: EscrowClient<'a>,
+    oracle: OracleClient<'a>,
     token: TokenClient<'a>,
     alice: Address,
     bob: Address,
