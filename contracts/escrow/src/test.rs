33daae2ce8743fda9c5169fcee8a8b3b66066e95
NanezX <victor.nanezj@gmail.com>
2024-04-16 23:23:19 -0400

    using wasm for both contracts

--- contracts/escrow/src/test.rs
+++ contracts/escrow/src/test.rs
@@ -1,38 +1,106 @@
+#![cfg(test)]
+
+extern crate std;
+
+use soroban_sdk::{
+    testutils::Address as _,
+    token::{Client as TokenClient, StellarAssetClient},
+    Address, Env,
+};
+
+pub mod oracle {
+    soroban_sdk::contractimport!(
+        file = "../../target/wasm32-unknown-unknown/release/signaturit_oracle.wasm"
+    );
+    pub type OracleClient<'a> = Client<'a>;
+}
+
+pub mod escrow {
+    soroban_sdk::contractimport!(file = "../../target/wasm32-unknown-unknown/release/escrow.wasm");
+    pub type EscrowClient<'a> = Client<'a>;
+}
+
+fn create_escrow_contract<'a>(env: &Env) -> escrow::EscrowClient<'a> {
+    let contract_id = env.register_contract_wasm(None, escrow::WASM);
+    escrow::EscrowClient::new(&env, &contract_id)
+}
+
+fn create_oracle_contract<'a>(env: &Env) -> oracle::OracleClient<'a> {
+    let contract_id = env.register_contract_wasm(None, oracle::WASM);
+    oracle::OracleClient::new(env, &contract_id)
+}
+
+fn create_token_contract<'a>(
+    env: &Env,
+    admin: &Address,
+) -> (TokenClient<'a>, StellarAssetClient<'a>) {
+    let contract_address = env.register_stellar_asset_contract(admin.clone());
+    (
+        TokenClient::new(env, &contract_address),
+        StellarAssetClient::new(env, &contract_address),
+    )
+}
+
+pub struct EscrowTest<'a> {
+    env: Env,
+    escrow: escrow::EscrowClient<'a>,
+    oracle: oracle::OracleClient<'a>,
+    token: TokenClient<'a>,
+    alice: Address,
+    bob: Address,
+    admin: Address,
+}
+
+impl<'a> EscrowTest<'a> {
+    fn setup() -> Self {
+        let test_setup = Self::setup_non_init();
+
+        // Initialize both contracts
+        test_setup.oracle.initialize(&test_setup.admin);
+
+        test_setup
+            .escrow
+            .initialize(&test_setup.token.address, &test_setup.oracle.address);
+
+        return test_setup;
+    }
+
+    fn setup_non_init() -> Self {
+        let env = Env::default();
+
+        // Generate the accounts (users)
+        let alice = Address::generate(&env);
+        let bob = Address::generate(&env);
+        let admin = Address::generate(&env);
+        assert_ne!(alice, bob);
+        assert_ne!(alice, admin);
+        assert_ne!(bob, admin);
+
+        // Create the contracts
+        let escrow_client = create_escrow_contract(&env);
+        let oracle_client = create_oracle_contract(&env);
+
+        let (token, token_admin) = create_token_contract(&env, &admin);
+
+        let amount: i128 = 1_000_000_000_000_000_000_000; // 1K tokens (18 decimals)
+
+        // Mint tokens for both addresses with both tokens
+        env.mock_all_auths();
+        token_admin.mint(&alice, &amount);
+        token_admin.mint(&bob, &amount);
+
+        return EscrowTest {
+            env,
+            escrow: escrow_client,
+            oracle: oracle_client,
+            token,
+            alice,
+            bob,
+            admin,
+        };
+    }
+}
+
 pub mod add_proposal;
 pub mod initialize;
 pub mod register;
-
-#[cfg(test)]
-mod utils {
-    use soroban_sdk::{token, Address, Env, String};
-    use token::Client as TokenClient;
-    use token::StellarAssetClient;
-
-    // keccak256(STOCKEN_ID_1)
-    pub const STOCKEN_ID_1: &str =
-        "6ef7e237bbddb133bb3504cad9e2ec7ff90c0c9b63567a632dbad8bb2b923728";
-    // keccak256(STOCKEN_ID_2)
-    pub const STOCKEN_ID_2: &str =
-        "af8f0b8ba4749d7a83edcd03a18e3ee3807fca630f8a18e8e59be53ea15c9e95";
-
-    pub fn create_token_contract<'a>(
-        e: &Env,
-        admin: &Address,
-    ) -> (TokenClient<'a>, StellarAssetClient<'a>) {
-        let contract_address = e.register_stellar_asset_contract(admin.clone());
-        (
-            TokenClient::new(e, &contract_address),
-            StellarAssetClient::new(e, &contract_address),
-        )
-    }
-
-    pub fn native_asset_contract_address(e: &Env) -> Address {
-        // CDF3YSDVBXV3QU2QSOZ55L4IVR7UZ74HIJKXNJMN4K5MOVFM3NDBNMLY
-        let str_address = String::from_str(
-            e,
-            "CDF3YSDVBXV3QU2QSOZ55L4IVR7UZ74HIJKXNJMN4K5MOVFM3NDBNMLY",
-        );
-
-        Address::from_string(&str_address)
-    }
-}
