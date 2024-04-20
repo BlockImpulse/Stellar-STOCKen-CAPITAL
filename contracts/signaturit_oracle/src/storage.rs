13c02cc7097636ccdd08ca0a787baece32695b05
NanezX <victor.nanezj@gmail.com>
2024-04-20 14:36:10 -0400

    unused storage structs

--- contracts/signaturit_oracle/src/storage.rs
+++ contracts/signaturit_oracle/src/storage.rs
@@ -8,27 +8,6 @@ pub trait Storage {
     fn remove(&self, env: &Env);
 }
 
-pub struct Instance;
-impl Instance {
-    pub fn get<K: IntoVal<Env, Val>, V: TryFromVal<Env, Val>>(env: &Env, key: &K) -> Option<V> {
-        env.storage().instance().get::<K, V>(key)
-    }
-    pub fn set<K: IntoVal<Env, Val>, V: IntoVal<Env, Val>>(env: &Env, key: &K, val: &V) {
-        env.storage().instance().set(key, val);
-    }
-    pub fn has<K: IntoVal<Env, Val>>(env: &Env, key: &K) -> bool {
-        env.storage().instance().has(key)
-    }
-    pub fn extend(env: &Env, min_ledger_to_live: u32) {
-        env.storage()
-            .instance()
-            .extend_ttl(min_ledger_to_live, min_ledger_to_live)
-    }
-    pub fn remove<K: IntoVal<Env, Val>>(env: &Env, key: &K) {
-        env.storage().instance().remove(key);
-    }
-}
-
 pub struct Persistent;
 impl Persistent {
     pub fn get<K: IntoVal<Env, Val>, V: TryFromVal<Env, Val>>(env: &Env, key: &K) -> Option<V> {
@@ -47,26 +26,5 @@ impl Persistent {
     }
     pub fn remove<K: IntoVal<Env, Val>>(env: &Env, key: &K) {
         env.storage().persistent().remove(key);
-    }
-}
-
-pub struct Temporary;
-impl Temporary {
-    pub fn get<K: IntoVal<Env, Val>, V: TryFromVal<Env, Val>>(env: &Env, key: &K) -> Option<V> {
-        env.storage().temporary().get::<K, V>(key)
-    }
-    pub fn set<K: IntoVal<Env, Val>, V: IntoVal<Env, Val>>(env: &Env, key: &K, val: &V) {
-        env.storage().temporary().set(key, val);
-    }
-    pub fn has<K: IntoVal<Env, Val>>(env: &Env, key: &K) -> bool {
-        env.storage().temporary().has(key)
-    }
-    pub fn extend<K: IntoVal<Env, Val>>(env: &Env, key: &K, min_ledger_to_live: u32) {
-        env.storage()
-            .temporary()
-            .extend_ttl(key, min_ledger_to_live, min_ledger_to_live)
-    }
-    pub fn remove<K: IntoVal<Env, Val>>(env: &Env, key: &K) {
-        env.storage().temporary().remove(key);
     }
 }
