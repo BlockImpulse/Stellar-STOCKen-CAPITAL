282e73b6b7d3f98b800016d449e64013078e2d6c
NanezX <victor.nanezj@gmail.com>
2024-05-01 18:28:01 -0400

    Oracle consumer trait

---
+++ libs/oracle_traits/src/lib.rs
@@ -0,0 +1,22 @@
+#![no_std]
+use soroban_sdk::{contractclient, Env, String};
+
+#[contractclient(name = "OracleConsumerClient")]
+pub trait OracleConsumer {
+    /**
+    Return a succesful response for a given signature process. This means that the
+    signature process was completed and the parites signed
+
+    # Arguments
+
+    * `signaturit_id` - The ID of the signature process that will be handled
+    * `document_hash` - The hash of the document hat was signed
+    */
+    fn completed_signature(env: Env, signaturit_id: String, document_hash: String);
+
+    /**
+    Return a failed response for a given signature process. This means that the
+    signature process was failed (parties declined, process was timeout, etc)
+    */
+    fn failed_signature(env: Env, signaturit_id: String);
+}
